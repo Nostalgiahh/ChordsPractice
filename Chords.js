3f8abf4beba8e7239a79ec573718740aace1d289
Nostalgia <kyleyuzon398@gmail.com>
2023-11-12 01:06:09 -0500

    progress...

---
+++ Chords.js
@@ -0,0 +1,14 @@
+document.getElementById("easy").addEventListener("click", function(){
+    let start = document.getElementById("cBox");
+    start.style.display = "none";
+  });
+  
+  document.getElementById("medium").addEventListener("click", function(){
+     let start = document.getElementById("cBox");
+    start.style.display = "none";
+  });
+  
+  document.getElementById("hard").addEventListener("click", function(){
+    let start = document.getElementById("cBox");
+    start.style.display = "none";
+  });
